9f5bb6f08c0cfb31d15da4719b2227243c3d26be
Rodrigo de França - DEV <159470915+rodrigodefrancadev@users.noreply.github.com>
2024-05-12 09:52:51 +0000

    mensagem

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <div>Olá Mundo!</div>
+    return <div style={{margin: 20}}>Olá Meu Amor! Bom dia. Se estiver preparada <a href="https://typebot.co/my-typebot-9syu8pl">Click Aqui</a></div>
 }
 
 export default Home;
