da7b7cddd8affe108afa3d45d13a7e80e6673090
Rodrigo de França - DEV <159470915+rodrigodefrancadev@users.noreply.github.com>
2024-05-14 12:29:13 +0000

    adicionado prettier

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-    return <div style={{margin: 20}}>Olá Meu Amor! Bom dia. Se estiver preparada <a href="https://typebot.co/my-typebot-9syu8pl">Click Aqui</a></div>
+  return (
+    <div style={{ margin: 20 }}>
+      Olá Meu Amor! Bom dia. Se estiver preparada{" "}
+      <a href="https://typebot.co/my-typebot-9syu8pl">Click Aqui</a>
+    </div>
+  );
 }
 
 export default Home;
