ccc5bb8686533654d7eb194984aa0c0c01c4daa5
Rodrigo de França - DEV <159470915+rodrigodefrancadev@users.noreply.github.com>
2024-05-26 18:59:20 +0000

    added `database opened_connections` in api/v1/status body

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -4,19 +4,25 @@ async function status(request, response) {
   const result = await database.query(
     "SELECT version() as version, current_setting('max_connections') as max_connections, (select count(*) from pg_stat_activity) as opened_connections",
   );
-  console.log(result.rows);
+
   const dbVersion = result.rows[0].version.split(" ")[1];
   const dbMaxConnections = parseInt(result.rows[0].max_connections);
+  const dbOpenedConnections = parseInt(result.rows[0].opened_connections);
+
   const updatedAt = new Date().toISOString();
-  response.status(200).json({
+
+  const responseBody = {
     updated_at: updatedAt,
     dependencies: {
       database: {
         version: dbVersion,
         max_connections: dbMaxConnections,
+        opened_connections: dbOpenedConnections,
       },
     },
-  });
+  };
+
+  response.status(200).json(responseBody);
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -2,7 +2,7 @@ test("Get api/v1/status and return status 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
 
   const body = await response.json();
-  console.log(body);
+
   expect(body.updated_at).toBeDefined();
 
   const parsedDate = new Date(body.updated_at);
@@ -11,4 +11,5 @@ test("Get api/v1/status and return status 200", async () => {
   expect(body.dependencies?.database).toBeDefined();
   expect(body.dependencies.database.version).toBeDefined();
   expect(body.dependencies.database.max_connections).toBeDefined();
+  expect(body.dependencies.database.opened_connections).toBeDefined();
 });
